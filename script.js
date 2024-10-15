19911be4f3bd7c2e750d29ac23153d7b1071b118
Bea Smith <bsmith152@ewu.edu>
2024-10-15 01:09:01 +0000

    updates to background,nav and more

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // Check JS is Connected
 console.log('I am present');
+
+// Mobile Menu
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Mobile Menu
+    function toggleMenu() {
+        const navLinks = document.querySelector('.navlinks');
+        if (navLinks.style.display === 'flex') {
+            navLinks.style.display = 'none';
+        } else {
+            navLinks.style.display = 'flex';
+            navLinks.style.flexDirection = 'column'; // Ensure it displays correctly
+        }
+    }
+
+    // Attach the toggleMenu function to the hamburger button
+    document.querySelector('.hamburger').addEventListener('click', toggleMenu);
+});
